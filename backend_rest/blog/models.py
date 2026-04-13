f00bd93398269f3a71dce1ae1bfdf5d43c9d16fa
Naman Sanotiya <sanotiyanaman18@gmail.com>
2026-04-14 02:28:27 +0530

    +comment + upvotes

--- backend_rest/blog/models.py
+++ backend_rest/blog/models.py
@@ -12,3 +12,28 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Comment(models.Model):
+    id = models.AutoField(primary_key=True)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name='comments')
+    U_ID = models.ForeignKey(User, on_delete=models.CASCADE, db_column='U_ID')
+    comment_text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'comment'
+
+    def __str__(self):
+        return f"Comment by {self.U_ID} on {self.blog}"
+ 
+class Upvote(models.Model):
+    id = models.AutoField(primary_key=True)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name='upvotes')
+    U_ID = models.ForeignKey(User, on_delete=models.CASCADE, db_column='U_ID')
+
+    class Meta:
+        db_table = 'upvote'
+        unique_together = ('blog', 'U_ID')#on one blog only one upvote from a user
+
+    def __str__(self):
+        return f"Upvote by {self.U_ID} on {self.blog}"
--- backend_rest/blog/urls.py
+++ backend_rest/blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.blog_list),         
     path('<int:pk>/', views.blog_detail),
+    path('<int:pk>/comment/', views.add_comment),
+    path('<int:pk>/upvote/', views.toggle_upvote),
 ]
